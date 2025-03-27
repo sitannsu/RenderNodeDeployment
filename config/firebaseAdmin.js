3bfb9532baf5731197eb99d0de749f39e15a7856
Sitansu <android.sitansu@gmail.com>
2025-03-27 17:57:44 +0530

    fcm code

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,7 +1,11 @@
 const admin = require('firebase-admin');
 
 // Initialize Firebase Admin with your service account credentials
-const serviceAccount = require('../firebase/serviceAccountKeys.json');
+require('dotenv').config({ path: './environments/.dev.env' });
+
+// Initialize Firebase Admin with service account from environment
+const serviceAccountStr = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+const serviceAccount = JSON.parse(serviceAccountStr.replace(/^'|'$/g, ''));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
